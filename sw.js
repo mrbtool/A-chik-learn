06c165a0eb3b83b808eca3a2725f82b030b2707a
mrbtools <124554185+mrbtool@users.noreply.github.com>
2026-02-01 08:42:07 +0530

    Update service worker cache version to v1.0.5

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // --- sw.js ---
 
 // 1. UPDATE VERSION HERE (Must match index.html version)
-const CACHE_NAME = "achik-learn-v1.0.4";
+const CACHE_NAME = "achik-learn-v1.0.5";
 
 const ASSETS = [
   "./",
