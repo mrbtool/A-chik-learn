acd0de7c2841a5c2bafa9f1779937638e83e95b1
mrbtools <124554185+mrbtool@users.noreply.github.com>
2026-02-07 00:31:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // --- sw.js ---
 
 // 1. UPDATE VERSION HERE (Must match index.html version)
-const CACHE_NAME = "achik-learn-v1.1.1";
+const CACHE_NAME = "achik-learn-v1.1.2";
 
 const ASSETS = [
   "./",
